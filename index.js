74d50cc1f32803aec98a38a7c3123402ef0d7abc
Favour Ojo <ojoore35@gmail.com>
2024-02-03 11:56:56 +0100

    minor refactoring

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
 
- 
-	form.addEventListener('submit', (e) => {
-        if(input.value == ""){
-            alert("Todo must not be blank");
-        } else{
-            e.preventDefault();
+    const updateLocalStorage = (tasks) => {
+        localStorage.setItem("todoItems", JSON.stringify(tasks));
+    };
 
-            const task = input.value;
+    const loadTasks = () => {
+        list_el.innerHTML = '';
+        const existingTodos = JSON.parse(localStorage.getItem("todoItems")) || [];
 
+        existingTodos.forEach((task, index) => {
             const task_el = document.createElement('div');
             task_el.classList.add('task');
 
@@ -30,7 +30,7 @@ window.addEventListener('load', () => {
 
             const task_actions_el = document.createElement('div');
             task_actions_el.classList.add('actions');
-            
+
             const task_edit_el = document.createElement('button');
             task_edit_el.classList.add('edit');
             task_edit_el.innerText = 'Edit';
@@ -39,7 +39,6 @@ window.addEventListener('load', () => {
             task_delete_el.classList.add('delete');
             task_delete_el.innerText = 'Delete';
 
-        
             task_actions_el.appendChild(task_edit_el);
             task_actions_el.appendChild(task_delete_el);
 
@@ -47,29 +46,52 @@ window.addEventListener('load', () => {
 
             list_el.appendChild(task_el);
 
-            input.value = '';
-
             task_content_el.addEventListener("click", (e) => {
                 task_content_el.classList.add("completed");
                 setTimeout(() => {
                     list_el.removeChild(task_el);
+                    existingTodos.splice(index, 1);
+                    updateLocalStorage(existingTodos);
                 }, 2000);
-            })
+            });
+
             task_edit_el.addEventListener('click', (e) => {
                 if (task_edit_el.innerText.toLowerCase() == "edit") {
                     task_edit_el.innerText = "Save";
-                    task_input_el.removeAttribute("readonly");
+                    task_input_el.removeAttribute("readon   ly");
                     task_input_el.focus();
                 } else {
                     task_edit_el.innerText = "Edit";
                     task_input_el.setAttribute("readonly", "readonly");
+                    existingTodos[index] = task_input_el.value;
+                    updateLocalStorage(existingTodos);
                 }
             });
 
             task_delete_el.addEventListener('click', (e) => {
                 list_el.removeChild(task_el);
+                existingTodos.splice(index, 1);
+                updateLocalStorage(existingTodos);
             });
+        });
+    };
+
+    form.addEventListener('submit', (e) => {
+        if (input.value == "") {
+            alert("Todo must not be blank");
+        } else {
+            e.preventDefault();
+
+            const task = input.value;
+            const existingTodos = JSON.parse(localStorage.getItem("todoItems")) || [];
+
+            existingTodos.push(task);
+            updateLocalStorage(existingTodos);
+
+            input.value = '';
+            loadTasks();
         }
-		
-	});
+    });
+
+    loadTasks();
 });
